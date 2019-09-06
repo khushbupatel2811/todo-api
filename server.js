dd018c31b38f4c8d5ce1668205b8027d2070a5a8
Khushbu Patel <patelkhushbu0035@gmail.com>
2019-09-06 19:06:34 +0530

    Use underscore methods

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var PORT = process.env.PORT || 3000;
 
 var todos = [];
@@ -32,12 +33,12 @@ app.get('/todos', function(req, res) {
 
 app.get('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo;
-    todos.forEach(function(todo) {
-        if (todoId === todo.id) {
-            matchedTodo = todo;
-        }
-    });
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    // todos.forEach(function(todo) {
+    //     if (todoId === todo.id) {
+    //         matchedTodo = todo;
+    //     }
+    // });
     if (matchedTodo) {
         res.json(matchedTodo);
     }
@@ -47,6 +48,11 @@ app.get('/todos/:id', function(req, res) {
 
 app.post('/todos', function(req, res) {
     var body = req.body;
+    body = _.pick(req.body, 'description', 'completed');
+    if (!_.isBoolean(body.completed) || !_.isString(body.description || body.description.trim().length === 0)) {
+        return res.status(400).send({ message: 'Invalid Input'});
+    }
+    body.description = body.description.trim();
     body.id = todoNextId++;
     todos.push(body);
     // console.log('description ' + body.description);
