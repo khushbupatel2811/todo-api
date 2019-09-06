cf56273fe43b57f26314ad40d845b59d7275e65b
Khushbu Patel <patelkhushbu0035@gmail.com>
2019-09-06 18:37:43 +0530

    Add POST to todos

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-    id: 1,
-    description: 'Meet mom for Lunch',
-    completed: false
-}, {
-    id: 2,
-    description: 'Go to market',
-    completed: false
-}, {
-    id: 3,
-    description: 'Pay the Bills',
-    completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
+// var todos = [{
+//     id: 1,
+//     description: 'Meet mom for Lunch',
+//     completed: false
+// }, {
+//     id: 2,
+//     description: 'Go to market',
+//     completed: false
+// }, {
+//     id: 3,
+//     description: 'Pay the Bills',
+//     completed: true
+// }];
 
 app.get('/', function(req, res) {
     res.send('Todo API root');
@@ -38,7 +43,15 @@ app.get('/todos/:id', function(req, res) {
     }
     res.status(404).send({ message: 'Resource NOT Found!'});
     // res.json('Get Single data using id ' + req.params.id);
-})
+});
+
+app.post('/todos', function(req, res) {
+    var body = req.body;
+    body.id = todoNextId++;
+    todos.push(body);
+    // console.log('description ' + body.description);
+    res.json(body);
+});
 
 app.listen(PORT, function() {
     console.log('Express listening on port ' + PORT + ' !');
