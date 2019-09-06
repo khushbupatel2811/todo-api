f0316b0b8d03b5a28592db42debeab5c70735432
Khushbu Patel <patelkhushbu0035@gmail.com>
2019-09-06 16:11:08 +0530

    fetch record by id

--- server.js
+++ server.js
@@ -2,9 +2,43 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+    id: 1,
+    description: 'Meet mom for Lunch',
+    completed: false
+}, {
+    id: 2,
+    description: 'Go to market',
+    completed: false
+}, {
+    id: 3,
+    description: 'Pay the Bills',
+    completed: true
+}];
+
 app.get('/', function(req, res) {
     res.send('Todo API root');
 });
+
+app.get('/todos', function(req, res) {
+    // res.send(JSON.stringify(todos));
+    res.json(todos);
+});
+
+app.get('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo;
+    todos.forEach(function(todo) {
+        if (todoId === todo.id) {
+            matchedTodo = todo;
+        }
+    });
+    if (matchedTodo) {
+        res.json(matchedTodo);
+    }
+    res.status(404).send({ message: 'Resource NOT Found!'});
+    // res.json('Get Single data using id ' + req.params.id);
+})
 
 app.listen(PORT, function() {
     console.log('Express listening on port ' + PORT + ' !');
